2acb4594473da4efeddd6caf0bf31b3207396a5d
klimkin-vlad <97385900+klimkin-vlad@users.noreply.github.com>
2022-01-10 13:49:49 +0300

    Add files via upload
    I made tests for validating methods / Я создал тесты для проверки методов

---
+++ BookCross/Book.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCross
+{
+    public class Book
+    {
+        string id;
+        string author;
+        string name;
+        int year;
+        int[] allIdPlaces;
+        int idReader;
+
+        public void Publish(string myName, string myAuthor, string year)
+        {
+            
+        }
+
+        public bool GetStatus()
+        {
+            return false;
+        }
+
+        public int GetIdReader()
+        {
+            return -1;
+        }
+
+        public void Take(int newId)
+        {
+
+        }
+
+        public bool Compare()
+        {
+            return false;
+        }
+
+        public string GetAuthor()
+        {
+            return author;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetYear()
+        {
+            return year.ToString();
+        }
+    }
+}
---
+++ BookCrossTest/BookTest.cs
@@ -0,0 +1,151 @@
+﻿using NUnit.Framework;
+using BookCross;
+
+namespace BookCrossTest
+{
+    [TestFixture]
+    public class BookTest
+    {
+        [Test]
+        public void PublishAll()
+        {
+            Book toTest = new Book();
+            toTest.Publish("Л. Н. Толстой", "Анна Каренина", "1877");
+            Assert.Equals(toTest.GetAuthor(), "Л. Н. Толстой");
+            Assert.Equals(toTest.GetName(), "Анна Каренина");
+            Assert.Equals(toTest.GetYear(), "1877");
+        }
+
+        [Test]
+        public void PublishNoYear()
+        {
+            Book toTest = new Book();
+            toTest.Publish("Л. Н. Толстой", "Анна Каренина", "");
+            Assert.Equals(toTest.GetAuthor(), "Л. Н. Толстой");
+            Assert.Equals(toTest.GetName(), "Анна Каренина");
+            Assert.Equals(toTest.GetYear(), "");
+        }
+
+        [Test]
+        public void PublishNoAuthor()
+        {
+            Book toTest = new Book();
+            toTest.Publish("Л. Н. Толстой", "", "");
+            Assert.Equals(toTest.GetAuthor(), "");
+            Assert.Equals(toTest.GetName(), "Анна Каренина");
+            Assert.Equals(toTest.GetYear(), "1877");
+        }
+
+        [Test]
+        public void PublishOnlyName()
+        {
+            Book toTest = new Book();
+            toTest.Publish("", "Анна Каренина", "");
+            Assert.Equals(toTest.GetAuthor(), "");
+            Assert.Equals(toTest.GetName(), "Анна Каренина");
+            Assert.Equals(toTest.GetYear(), "");
+        }
+
+        [Test]
+        public void GetStatusTrue()
+        {
+            Book toTest = new Book();
+            toTest.Publish("Л. Н. Толстой", "Анна Каренина", "1877");
+            toTest.Take(1);
+            Assert.IsTrue(toTest.GetStatus());
+        }
+
+        public void GetStatusFalse()
+        {
+            Book toTest = new Book();
+            toTest.Publish("Л. Н. Толстой", "Анна Каренина", "1877");
+            Assert.IsFalse(toTest.GetStatus());
+        }
+
+        [Test]
+        public void GetReaderTrue()
+        {
+            Book toTest = new Book();
+            toTest.Publish("Л. Н. Толстой", "Анна Каренина", "1877");
+            toTest.Take(1);
+            Assert.Equals(toTest.GetIdReader(), 1);
+        }
+
+        public void GetReaderFalse()
+        {
+            Book toTest = new Book();
+            toTest.Publish("Л. Н. Толстой", "Анна Каренина", "1877");
+            Assert.Equals(toTest.GetIdReader(), -1);
+        }
+
+        [Test]
+        public void CompareSame()
+        {
+            Book toTest = new Book();
+            Book toTest1 = new Book();
+            toTest.Publish("Л. Н. Толстой", "Анна Каренина", "1877");
+            toTest1.Publish("Л. Н. Толстой", "Анна Каренина", "1877");
+            Assert.IsTrue(toTest.Compare());
+        }
+
+        [Test]
+        public void CompareNoAuthor()
+        {
+            Book toTest = new Book();
+            Book toTest1 = new Book();
+            toTest.Publish("Л. Н. Толстой", "Анна Каренина", "1877");
+            toTest1.Publish("", "Анна Каренина", "1877");
+            Assert.IsFalse(toTest.Compare());
+        }
+
+        [Test]
+        public void CompareNoName()
+        {
+            Book toTest = new Book();
+            Book toTest1 = new Book();
+            toTest.Publish("Л. Н. Толстой", "Анна Каренина", "1877");
+            toTest1.Publish("Л. Н. Толстой", "", "1877");
+            Assert.IsFalse(toTest.Compare());
+        }
+
+        [Test]
+        public void CompareNoYear()
+        {
+            Book toTest = new Book();
+            Book toTest1 = new Book();
+            toTest.Publish("Л. Н. Толстой", "Анна Каренина", "1877");
+            toTest1.Publish("Л. Н. Толстой", "Анна Каренина", "");
+            Assert.IsFalse(toTest.Compare());
+        }
+
+        [Test]
+        public void CompareOnlyName()
+        {
+            Book toTest = new Book();
+            Book toTest1 = new Book();
+            toTest.Publish("Л. Н. Толстой", "Анна Каренина", "1877");
+            toTest1.Publish("", "Анна Каренина", "");
+            Assert.IsFalse(toTest.Compare());
+        }
+
+        [Test]
+        public void CompareOnlyAuthor()
+        {
+            Book toTest = new Book();
+            Book toTest1 = new Book();
+            toTest.Publish("Л. Н. Толстой", "Анна Каренина", "1877");
+            toTest1.Publish("Л. Н. Толстой", "", "");
+            Assert.IsFalse(toTest.Compare());
+        }
+
+        [Test]
+        public void CompareOnlyYear()
+        {
+            Book toTest = new Book();
+            Book toTest1 = new Book();
+            toTest.Publish("Л. Н. Толстой", "Анна Каренина", "1877");
+            toTest1.Publish("", "", "1877");
+            Assert.IsFalse(toTest.Compare());
+        }
+    }
+}
