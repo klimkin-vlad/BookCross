b19995e1e74b30219cea0d26698a08932dda2e92
klimkin-vlad <97385900+klimkin-vlad@users.noreply.github.com>
2022-01-11 12:10:55 +0300

    New tests / Новые тесты
    Added test for new class / Добавлены тесты для нового класса

---
+++ BookCross/Books.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCross
+{
+    public class Books
+    {
+        List<Book> allBooks;
+
+        public void Publish(int myId, string myAuthor, string myName, string myYear)
+        {
+
+        }
+
+        public bool GetStatus(int myId)
+        {
+            return false;
+        }
+
+        public int GetIdReader(int myId)
+        {
+            return -1;
+        }
+
+        public void Take(int idBook, int idReader, int idPlace)
+        {
+
+        }
+
+        public bool Compare(int first, int second)
+        {
+            return false;
+        }
+
+        public string GetAuthor(int myId)
+        {
+            return "";
+        }
+
+        public string GetName(int myId)
+        {
+            return "";
+        }
+
+        public string GetYear(int myId)
+        {
+            return "";
+        }
+
+        public List<string> GetAll(int myId)
+        {
+            List<string> dataBook = new List<string>();
+            return dataBook;
+        }
+
+        public List<int> SearchByAuthor(string myAuthor)
+        {
+            List<int> ids = new List<int>();
+            return ids;
+        }
+
+        public List<int> SearchByName(string myName)
+        {
+            List<int> ids = new List<int>();
+            return ids;
+        }
+
+        public List<int> SearchByYear(string myYear)
+        {
+            List<int> ids = new List<int>();
+            return ids;
+        }
+    }
+}
---
+++ BookCrossTest/BooksTest.cs
@@ -0,0 +1,207 @@
+﻿using NUnit.Framework;
+using BookCross;
+using System.Collections.Generic;
+
+namespace BookCrossTest
+{
+    [TestFixture]
+    public class BooksTest
+    {
+        [Test]
+        public void PublishAll()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            List<string> bookData = allBooks.GetAll(1);
+            Assert.AreEqual(bookData[0], "1");
+            Assert.AreEqual(bookData[1], "Л. Н. Толстой");
+            Assert.AreEqual(bookData[2], "Анна Каренина");
+            Assert.AreEqual(bookData[3], "1877");
+        }
+
+        [Test]
+        public void PublishNoYear()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "");
+            List<string> bookData = allBooks.GetAll(1);
+            Assert.AreEqual(bookData[0], "1");
+            Assert.AreEqual(bookData[1], "Л. Н. Толстой");
+            Assert.AreEqual(bookData[2], "Анна Каренина");
+            Assert.AreEqual(bookData[3], "");
+        }
+
+        public void PublishNoAuthor()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "", "Анна Каренина", "1877");
+            List<string> bookData = allBooks.GetAll(1);
+            Assert.AreEqual(bookData[0], "1");
+            Assert.AreEqual(bookData[1], "");
+            Assert.AreEqual(bookData[2], "Анна Каренина");
+            Assert.AreEqual(bookData[3], "1877");
+        }
+
+        public void PublishOnlyName()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "", "Анна Каренина", "");
+            List<string> bookData = allBooks.GetAll(1);
+            Assert.AreEqual(bookData[0], "1");
+            Assert.AreEqual(bookData[1], "");
+            Assert.AreEqual(bookData[2], "Анна Каренина");
+            Assert.AreEqual(bookData[3], "");
+        }
+
+        [Test]
+        public void GetStatusTrue()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            allBooks.Take(1, 1, 1);
+            Assert.IsTrue(allBooks.GetStatus(1));
+        }
+
+        [Test]
+        public void GetStatusFalse()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            Assert.IsFalse(allBooks.GetStatus(1));
+        }
+
+        [Test]
+        public void GetReaderTrue()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            allBooks.Take(1, 1, 1);
+            Assert.AreEqual(allBooks.GetIdReader(1), 1);
+        }
+
+        public void GetReaderFalse()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            Assert.AreEqual(allBooks.GetIdReader(1), -1);
+        }
+
+        [Test]
+        public void CompareSame()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            allBooks.Publish(2, "Л. Н. Толстой", "Анна Каренина", "1877");
+            Assert.IsTrue(allBooks.Compare(1, 2));
+        }
+
+        [Test]
+        public void CompareNoAuthor()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            allBooks.Publish(2, "", "Анна Каренина", "1877");
+            Assert.IsFalse(allBooks.Compare(1, 2));
+        }
+
+        [Test]
+        public void CompareNoName()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            allBooks.Publish(2, "Л. Н. Толстой", "", "1877");
+            Assert.IsFalse(allBooks.Compare(1, 2));
+        }
+
+        [Test]
+        public void CompareNoYear()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            allBooks.Publish(2, "Л. Н. Толстой", "Анна Каренина", "");
+            Assert.IsFalse(allBooks.Compare(1, 2));
+        }
+
+        [Test]
+        public void CompareOnlyName()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            allBooks.Publish(2, "", "Анна Каренина", "");
+            Assert.IsFalse(allBooks.Compare(1, 2));
+        }
+
+        [Test]
+        public void CompareOnlyAuthor()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            allBooks.Publish(2, "Л. Н. Толстой", "", "");
+            Assert.IsFalse(allBooks.Compare(1, 2));
+        }
+
+        [Test]
+        public void CompareOnlyYear()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            allBooks.Publish(2, "", "", "1877");
+            Assert.IsTrue(allBooks.Compare(1, 2));
+        }
+
+        [Test]
+        public void GetAuthor()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            Assert.AreEqual(allBooks.GetAuthor(1), "Л. Н. Толстой");
+        }
+
+        [Test]
+        public void GetName()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            Assert.AreEqual(allBooks.GetName(1), "Анна Каренина");
+        }
+
+        [Test]
+        public void GetYear()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            Assert.AreEqual(allBooks.GetYear(1), "1877");
+        }
+
+        [Test]
+        public void SearchByAuthor()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            allBooks.Publish(2, "Л. Н. Толстой", "Война и мир", "1873");
+            List<int> allIds = allBooks.SearchByAuthor("Л. Н. Толстой");
+            Assert.AreEqual(allIds[0], 1);
+            Assert.AreEqual(allIds[1], 2);
+        }
+
+        [Test]
+        public void SearchByName()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            allBooks.Publish(2, "Л. Н. Толстой", "Война и мир", "1873");
+            List<int> allIds = allBooks.SearchByName("Анна Каренина");
+            Assert.AreEqual(allIds[0], 1);
+        }
+
+        [Test]
+        public void SearchByYear()
+        {
+            Books allBooks = new Books();
+            allBooks.Publish(1, "Л. Н. Толстой", "Анна Каренина", "1877");
+            allBooks.Publish(2, "Л. Н. Толстой", "Война и мир", "1873");
+            List<int> allIds = allBooks.SearchByYear("1877");
+            Assert.AreEqual(allIds[0], 1);
+        }
+    }
+}
